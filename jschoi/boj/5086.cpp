ee249107643a3be43fd12e0977220b108bb25d36
jinsukuku <jinsukuku@gmail.com>
2025-04-28 23:13:11 +0900

    Sloved: Boj 5086;

---
+++ jschoi/boj/5086.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n1 = 0;
+    int n2 = 0;
+
+    while(1){
+        cin >> n1 >> n2;
+        if(cin.eof()) break;
+        if(n1 == 0 || n2 == 0) break;
+        if(n2 % n1 == 0) cout << "factor" << endl;
+        else if(n1 % n2 == 0) cout << "multiple" << endl;
+        else cout << "neither" << endl;
+    }
+    return 0;
+}
+
+
+
+/*
+cpp 에서 EOF 처리
+while(1){
+    int n = 0;
+    cin >> n;
+    if(cin.eof()) break;
+    ...
+}
+*/
