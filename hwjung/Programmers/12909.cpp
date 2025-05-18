1dc5c3c71b6e48368f6ac1002fbb8dbfd17357ca
Hae1Won1 <h121581@naver.com>
2025-05-18 17:23:37 +0900

    Solve SP5379

---
+++ hwjung/Programmers/12909.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include<stack>
+#include<string>
+using namespace std;
+
+//C++ 풀이로 변경함에 따라 매개변수 string으로 변경해주어야함
+bool solution(string s) {
+    
+    stack<char> st;
+
+    for (char c : s) {
+        if (c == '(') {
+            st.push(c);
+        }
+        else {
+            if (st.empty()) {
+                return false;
+            }
+            st.pop();
+        }
+    }
+    return st.empty();
+}
+
+//범위기반 for문 사용
+//일반 for문에 비해 안전하고 간단하지만 index정보가 없다는 단점 존재
+
---
+++ hwjung/boj/5397.cpp
@@ -0,0 +1,55 @@
+﻿#include<iostream>
+#include<string>
+#include<stack>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	//n번 반복
+	for (int i = 0; i < n; i++) {
+		stack<char> st1, st2;
+		string s = "";
+		cin >> s;
+
+		for (char c : s) {
+			// < 일 경우  st1이 비어있지 않을 경우 st1의 top을 st2에 push하고 st1을 pop한다
+			if (c == '<') {
+				if (!(st1.empty())) {
+					st2.push(st1.top());
+					st1.pop();
+				}
+			}
+			// > 일 경우 st2가 비어있지 않을 경우 st2의 top을 st1에 push하고 st2를 pop한다
+			else if (c == '>') {
+				if (!(st2.empty())) {
+					st1.push(st2.top());
+					st2.pop();
+				}
+			}
+			// - 일 경우  st1이 비어있지 않을 경우 st1을 pop한다
+			else if (c == '-') {
+				if (!(st1.empty())) {
+					st1.pop();
+				}
+			}
+			//이외 문자는 st1에 push한다
+			else {
+				st1.push(c);
+			}
+		}
+
+		//입력이 모두 끝나고나면 st1을 empty가 true가 될때까지 top을 st2에 push한다
+		while (!(st1.empty())) {
+			st2.push(st1.top());
+			st1.pop();
+		}
+
+		while (!(st2.empty())) {
+			cout << st2.top();
+			st2.pop();
+		}
+		cout << '\n';
+	}
+}
