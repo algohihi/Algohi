771b4498555ce33673791085397ebcf9854f2467
jinsukuku <jinsukuku@gmail.com>
2025-05-20 07:49:59 +0900

    Slove: 12909 올바른 괄호

--- jschoi/programmers/12909.cpp
+++ jschoi/programmers/12909.cpp
@@ -1,14 +1,12 @@
+#include <string>
 #include <iostream>
 #include <stack>
-#include <string>
 
 using namespace std;
 
-bool solution(const string& s) {
+bool solution(string s)
+{
     stack<char> st;
-
-    if (s[0] == ')') return false;
-
     for (char ch : s) {
         if (ch == '(') {
             st.push(ch);
@@ -19,17 +17,4 @@ bool solution(const string& s) {
     }
 
     return st.empty();
-}
-
-int main() {
-    string input;
-    cin >> input;
-
-    if (solution(input)) {
-        cout << "true\n";
-    } else {
-        cout << "false\n";
-    }
-
-    return 0;
 }
