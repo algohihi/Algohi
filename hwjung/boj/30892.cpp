63810ba1a1e4cdf0550babacab554863c84614c1
Hae1Won1 <h121581@naver.com>
2025-06-09 18:16:50 +0900

    Wip: boj30892

--- hwjung/boj/30892.cpp
+++ hwjung/boj/30892.cpp
@@ -8,19 +8,59 @@
 */
 #include<iostream>
 #include<vector>
+#include<algorithm>
+#include<stack>
 using namespace std;
 
+// 미래에 다시 풀어볼게요...
+
+/* K번 먹는다 할때, 최적의 수가 되지 않음
 int main() {
+	long long n, k, t;
+	cin >> n >> k >> t;
+	long long* a = new long long[n];
 
+	// 상어들의 크기를 받음
+	for (int i = 0; i < n; i++) {
+		cin >> *(a + i);
+	}
 
+	sort(a, a + n);
+	stack<long long> st;
+	
+	//t보다 큰 애들 st에 저장, 먹을 수 있는 애들 중 가장 큰 얘 얌
+	for (int i = n - 1; i >= 0; i--) {
+		if ( t <= a[i]) {
+			st.push(a[i]);
+		}
+		else {
+			t += a[i];
+			k--;
+			//break;
+		}
+	}
+	
 
-
+	// pop으로인해 최대개수 k번째까지 못 먹을 수 있음
+	for (int i = 0; i < k; i++) {
+		bool ate = false;
+		long long max = 0;
+		while (!st.empty() && t > st.top()) {
+			max = st.top();
+			st.pop();
+			ate = true;
+		}
+		if (ate) t += max;
+		else break; // 더 이상 먹을 수 있는 상어가 없음
+	}
+	cout << t;
+	delete[] a;
 }
-
+*/
 
 
 // 상단 과정 그대로 진행
-// 시간 초과 : stack만으로 푸는 게 가능한가...??? 
+// 시간 초과
 /*
 #include<iostream>
 #include<stack>
