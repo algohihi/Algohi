006273e2013246ebeba7d31052074deb23c04c5b
MJ37031 <rlaalswl0520@naver.com>
2025-08-25 08:42:05 +0900

    Solve: 2225

--- mjkim/Boj/2133.cpp
+++ mjkim/Boj/2133.cpp
@@ -2,43 +2,29 @@
 
 using namespace std;
 
+int main() 
+{
+    int n, k;  // 합이 N, 개수 K
+    cin >> n >> k;
 
-int fill(int n) {
+    int d[201][201] = { 0 };
+    d[0][0] = 1;
 
-	if (n % 2 == 1)  // 홀수거나 n-2가 0보다 작을 때는 0개
-	{
-		return 0;
-	}
-	else if (n == 0)
-	{
-		return 1;
-	}
-
-	else if (n == 2)  // 3*2 일 때는 3 리턴
-	{
-		return 3;
-	}
-
-	else
-	{
-		int tile = 3 * fill(n - 2);
-
-		for (int i = 4; i <= n; i += 2)
-		{
-			tile += 2 * fill(n - i);
-		}
-
-		return tile;
-	}
+    for (int i = 1; i <= k; i++) // k개
+    {
+        for (int j = 0; j <= n; j++) // 합이 n이 되도록
+        {
+            // 마지막 자리에 오는 수를 l이라고 했을 때 그 앞에 오는 수들의 합은 n-l
+            for (int l = 0; l <= j; l++)  
+            {
+                d[i][j] += d[i - 1][j - l];
+                d[i][j] %= 1000000000;
+            }
+        }
+    }
 
 
-}
+    cout << d[k][n] << '\n';
 
-int main() {
-	int n;
-	cin >> n;
-
-	cout << fill(n) << '\n';
-
-	return 0;
+    return 0;
 }
