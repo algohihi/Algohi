748f28ef08ad8135d14dab59face1b0fd59d2720
MJ37031 <rlaalswl0520@naver.com>
2025-09-01 20:23:20 +0900

    Solve: 2309

--- 2309.cpp
+++ 2309.cpp
@@ -3,9 +3,9 @@
 
 using namespace std;
 
-int main() 
+int main()
 {
-	int height[10];
+	int height[9];
 	int sum = 0;
 
 	// 난쟁이들 키 입력
@@ -15,26 +15,28 @@ int main()
 		sum += height[i];  // 키를 모두 sum에 합해서 저장
 	}
 
-	sort(height, height+9);  // 오름차순 정렬
+	sort(height, height + 9);  // 오름차순 정렬
 
 	// 9명 중에 7명 선택
 	for (int i = 0; i < 9; i++)
 	{
-		for (int j = i; j < 9; j++) {
+		for (int j = i + 1; j < 9; j++) {
 
 			// 9명 중에서 2명 뺀 값이 100이면
 			if (sum - height[i] - height[j] == 100)
+			{
 				for (int k = 0; k < 9; k++)
 				{
-					
 					// 그 두명 빼고 출력
 					if (k == i || k == j)
 					{
 						continue;
 					}
-					else
-						cout << height[k] << '\n';
+
+					cout << height[k] << '\n';
 				}
+				return 0;
+			}
 		}
 	}
 	return 0;
